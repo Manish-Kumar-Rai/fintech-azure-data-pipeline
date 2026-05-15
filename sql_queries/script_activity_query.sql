61d6d7cd19cf259f0e3644da2bb1d575ef891931
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2026-05-15 07:15:10 +0200

    sql queries for various activity

---
+++ sql_queries/script_activity_query.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*) AS tgt_count
+FROM OPENROWSET(
+    BULK 'bronze/@{item().SchemaName}/@{item().TableName}/*.parquet',
+    DATA_SOURCE = 'FintechDataExternal',
+    FORMAT = 'PARQUET'
+) AS rows;
